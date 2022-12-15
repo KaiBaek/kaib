249aa6eb8b4f50d6bbeb5860220549b992429fa5
KaiBaek <bjy050602@gmail.com>
2022-12-14 21:48:33 -0800

    #

--- BJ1406.cpp
+++ BJ1406.cpp
@@ -1,5 +1,36 @@
 /* 
 Simple Editor
+Olympiad > Croatian Highschool Competitions in Informatics > 2004 > National Competition #1 - Juniors 
+
+Problem #2
+
+You are given a text that is a sequence of characters.
+
+Cursor can be positioned inside of the text (between any two consecutive characters), at the beginning (left of the first character) or at the end (right of the last character) of the text.
+
+You are given sequence of operations you must perform on the text. 
+
+Possible operations are: 
+
+L	move cursor one character to the left (if cursor is at the beginning, do nothing)
+D	move cursor one character to the right (if cursor is at the end, do nothing)
+B	delete character left of the cursor (if cursor is at the beginning, do nothing)
+P $	add character $ right of the cursor (character $ is any lowercase letter of English alphabet)
+Before execution of given operations, cursor is at the end of the text. 
+
+Write a program that will determine what would the text look like after execution of given operations. 
+
+Input
+
+In the first row there is the text. It consists only of lowercase letters of English alphabet and its maximal length is 100,000 characters.
+
+In the next row there is an integer N, 1 ≤ N ≤ 500,000, number of given operations. 
+
+In the next N rows there are operations given in the order of execution. 
+
+Output
+
+In first and only row you should write text after the execution of all the operations. 
 */
 
 
